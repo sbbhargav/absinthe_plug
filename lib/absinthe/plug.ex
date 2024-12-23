6abdb4a7bbb9fae10723d990e6ceb46680a42d68
sbbhargav <sb.bhargav@kreeti.com>
2024-12-23 16:45:11 +0530

    update token_limit

--- lib/absinthe/plug.ex
+++ lib/absinthe/plug.ex
@@ -228,6 +228,12 @@ defmodule Absinthe.Plug do
     schema_mod = opts |> get_schema
 
     raw_options = Keyword.take(opts, @raw_options)
+
+    raw_options =
+      Keyword.update(raw_options, :token_limit, :infinity, fn _ ->
+        Keyword.get(opts, :token_limit, :infinity)
+      end)
+
     log_level = Keyword.get(opts, :log_level, :debug)
 
     pubsub = Keyword.get(opts, :pubsub, nil)
